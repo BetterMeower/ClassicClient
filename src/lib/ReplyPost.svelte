df6023d6addc93f0e8686ef4625f1922c1e24428
Bloctans <93960460+Bloctans@users.noreply.github.com>
2023-07-17 23:22:42 -0500

    reply chains are now working sorta

--- src/lib/ReplyPost.svelte
+++ src/lib/ReplyPost.svelte
@@ -2,22 +2,29 @@
 <script>
 	import Container from "./Container.svelte";
     import {apiUrl} from "./urls.js";
+	import {auth_header} from "./stores.js"
 
 	import ReplyPost from "./ReplyPost.svelte"
 
 	import {onMount, tick} from "svelte";
 
 	export let post;
+	export let ShowMoreButton = true
+
+	let _ShowMore = false
+
+	let bridged = false
+	let webhook = false
 
     function initPostUser() {
         if (!post.user) return;
 
 		if (post.content.includes(":")) {
-			let bridged =
+			bridged =
 				post.user === "Discord" ||
 				post.user === "revolt" ||
 				post.user === "Revower";
-			let webhook = post.user == "Webhooks";
+			webhook = post.user == "Webhooks";
 		}
 
 		if (bridged || webhook) {
@@ -30,7 +37,7 @@
 </script>
 
 <Container>
-    {#await fetch(`${apiUrl}posts?id=${post}`).then(res => res.json())}
+    {#await fetch(`${apiUrl}posts?id=${post}`,{headers: {"Authorization":$auth_header}}).then(res => res.json())}
         <span class="loading">
             <span class="circle circle1"></span>
             <span class="circle circle2"></span>
@@ -46,8 +53,18 @@
             <span><b>{info.p.split(": ")[0]}</b> {info.p.slice(info.p.indexOf(": ") + 1)}</span>
         {:else}
 			{#if info.p.search(/^@\w+\s\[\w+-\w+-\w+-\w+-\w+\]\s*/i) != -1}
-				<br>
-				<ReplyPost post={info.p.split(" ").splice(1, 1)[0].replace("[", "").replace("]", "")} />
+				{#if ShowMoreButton}
+					{#if !_ShowMore}
+						<button class="full" on:click={() => {_ShowMore = true}}>Show Full reply chain</button>
+						<br>
+						<br>
+					{/if}
+					{#if _ShowMore}
+						<ReplyPost post={info.p.split(" ").splice(1, 1)[0].replace("[", "").replace("]", "")} ShowMoreButton = {false}  />
+					{/if}
+				{:else}
+					<ReplyPost post={info.p.split(" ").splice(1, 1)[0].replace("[", "").replace("]", "")} ShowMoreButton = {false}  />
+				{/if}
 				<span><b>{info.u}</b> {info.p.split(/^@\w+\s\[\w+-\w+-\w+-\w+-\w+\]\s*/i).join(" ")}</span>
 			{:else}
 				<span><b>{info.u}</b> {info.p}</span>
@@ -89,6 +106,10 @@
     .text {
         margin-left: 6px;
     }
+
+	.full {
+		width:100%;
+	}
 
 	@keyframes jump {
 		from {
